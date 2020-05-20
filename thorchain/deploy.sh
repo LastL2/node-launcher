92432c3c244550fb7187df5584b4d2f430057517
Fandral <fandral.dev@gmail.com>
2020-05-20 15:51:46 +1000

    Fix p2p thor-daemon

--- thorchain/deploy.sh
+++ thorchain/deploy.sh
@@ -5,13 +5,14 @@
 
 set -e
 
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 RELEASE=${1:-mainnet}
 NODES=${2:-1}
 PEER=${3:-thor-daemon.$RELEASE-1}
 PEER_API=${4:-thor-api.$RELEASE-1}
 
 if [ $RELEASE != "mainnet" ]; then
-  FILE="-f $RELEASE.yaml"
+  FILE="-f $SCRIPTPATH/$RELEASE.yaml"
 fi
 
 for i in `seq 1 $NODES`
@@ -19,21 +20,30 @@ do
 		if [ $i = 1 ]
     then
       echo Deploying $RELEASE in namespace $RELEASE-$i
-			helm upgrade --install $RELEASE . -n $RELEASE-$i --create-namespace $FILE
+			helm upgrade --install $RELEASE $SCRIPTPATH -n $RELEASE-$i --create-namespace $FILE --set bifrost.binanceStartBlockHeight=1
 		else
 			MNEMONIC=$(docker run namick/12-words)
-			cat << EOF > tmp.yaml
+			cat << EOF > $SCRIPTPATH/tmp.yaml
 thor-daemon:
   peer: $PEER
   peerApi: $PEER_API
   signer:
     mnemonic: $MNEMONIC
 bifrost:
+  binanceDaemon: binance-daemon.mocknet-1:26660
+  bitcoinDaemon: bitcoin-daemon.mocknet-1:18443
+  ethereumDaemon: ethereum-daemon.mocknet-1:8545
   signer:
     mnemonic: $MNEMONIC
+binance-daemon:
+  enabled: false
+bitcoin-daemon:
+  enabled: false
+ethereum-daemon:
+  enabled: false
 EOF
       echo Deploying $RELEASE in namespace $RELEASE-$i
-			helm upgrade --install $RELEASE . -n $RELEASE-$i --create-namespace $FILE -f tmp.yaml
-      rm tmp.yaml
+			helm upgrade --install $RELEASE $SCRIPTPATH -n $RELEASE-$i --create-namespace $FILE -f $SCRIPTPATH/tmp.yaml
+      rm $SCRIPTPATH/tmp.yaml
 		fi
 done
