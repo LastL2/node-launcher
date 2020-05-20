57161bc7208b3aa8a284f5b0b8660a39a86391ab
Fandral <fandral.dev@gmail.com>
2020-05-20 18:30:54 +1000

    Update bifrost service for TSS comms

--- thorchain/deploy.sh
+++ thorchain/deploy.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 # deploy.sh
 # deploy.sh testnet
-# deploy.sh mocknet 12 1.2.3.4 2.3.4.5
+# deploy.sh mocknet 5 123.123.123.123
 
 set -e
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 RELEASE=${1:-mainnet}
 NODES=${2:-1}
-PEER=${3:-thor-daemon.$RELEASE-1}
-PEER_API=${4:-thor-api.$RELEASE-1}
+PEER=$3
 
 if [ $RELEASE != "mainnet" ]; then
   FILE="-f $SCRIPTPATH/$RELEASE.yaml"
@@ -17,22 +16,26 @@ fi
 
 for i in `seq 1 $NODES`
 do
+    NODE=$RELEASE-$i
+    echo Deploying $NODE
 		if [ $i = 1 ]
     then
-      echo Deploying $RELEASE in namespace $RELEASE-$i
-			helm upgrade --install $RELEASE $SCRIPTPATH -n $RELEASE-$i --create-namespace $FILE --set bifrost.binanceStartBlockHeight=1
+			helm upgrade --install $RELEASE $SCRIPTPATH -n $NODE --create-namespace $FILE --set bifrost.binanceStartBlockHeight=1
 		else
+      PEER_NODE=$RELEASE-1
 			MNEMONIC=$(docker run namick/12-words)
+      # TODO create functions to handle config file for testnet
 			cat << EOF > $SCRIPTPATH/tmp.yaml
 thor-daemon:
-  peer: $PEER
-  peerApi: $PEER_API
+  peer: ${PEER:-thor-daemon.$PEER_NODE}
+  peerApi: ${PEER:-thor-api.$PEER_NODE}
   signer:
     mnemonic: $MNEMONIC
 bifrost:
-  binanceDaemon: binance-daemon.mocknet-1:26660
-  bitcoinDaemon: bitcoin-daemon.mocknet-1:18443
-  ethereumDaemon: ethereum-daemon.mocknet-1:8545
+  peer: ${PEER:-bifrost.$PEER_NODE}
+  binanceDaemon: ${PEER:-binance-daemon.$PEER_NODE:26660}
+  bitcoinDaemon: ${PEER:-bitcoin-daemon.$PEER_NODE:18443}
+  ethereumDaemon: ${PEER:-ethereum-daemon.$PEER_NODE:8545}
   signer:
     mnemonic: $MNEMONIC
 binance-daemon:
@@ -42,8 +45,7 @@ bitcoin-daemon:
 ethereum-daemon:
   enabled: false
 EOF
-      echo Deploying $RELEASE in namespace $RELEASE-$i
-			helm upgrade --install $RELEASE $SCRIPTPATH -n $RELEASE-$i --create-namespace $FILE -f $SCRIPTPATH/tmp.yaml
+			helm upgrade --install $RELEASE $SCRIPTPATH -n $NODE --create-namespace $FILE -f $SCRIPTPATH/tmp.yaml
       rm $SCRIPTPATH/tmp.yaml
 		fi
 done
