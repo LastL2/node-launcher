7d64d2a1a9501baed978e58af3b76eef955df450
Fandral <fandral.dev@gmail.com>
2020-05-21 21:36:52 +1000

    Update templates

--- Makefile
+++ Makefile
@@ -1,1 +1,7 @@
 include thorchain/Makefile
+
+logs:
+	helm upgrade elastic ./elastic-operator --install -n elastic-system --create-namespace
+
+metrics:
+	helm upgrade prometheus stable/prometheus-operator --install -n prometheus --create-namespace -f prometheus/values.yaml
--- thorchain/deploy.sh
+++ thorchain/deploy.sh
@@ -1,51 +1,123 @@
-#!/bin/sh
-# deploy.sh
-# deploy.sh testnet
-# deploy.sh mocknet 5 123.123.123.123
+#!/usr/bin/env sh
 
 set -e
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-RELEASE=${1:-mainnet}
-NODES=${2:-1}
-PEER=$3
 
-if [ $RELEASE != "mainnet" ]; then
-  FILE="-f $SCRIPTPATH/$RELEASE.yaml"
-fi
+NET=${NET:-mocknet}
+NODES=${NODES:-1}
 
-for i in `seq 1 $NODES`
-do
-    NODE=$RELEASE-$i
-    echo Deploying $NODE
-		if [ $i = 1 ]
-    then
-			helm upgrade --install $RELEASE $SCRIPTPATH -n $NODE --create-namespace $FILE --set bifrost.binanceStartBlockHeight=1
-		else
-      PEER_NODE=$RELEASE-1
-			MNEMONIC=$(docker run namick/12-words)
-      # TODO create functions to handle config file for testnet
-			cat << EOF > $SCRIPTPATH/tmp.yaml
+
+VALUES=$SCRIPTPATH/$NET.yaml
+VALUES_MNEMONIC=$SCRIPTPATH/tmp.yaml
+VALUES_VALIDATOR=$SCRIPTPATH/$NET-validator.yaml
+
+create_mnemonic_config () {
+  VALUE=${MNEMONIC:-$(docker run namick/12-words)}
+  cat << EOF > $VALUES_MNEMONIC
 thor-daemon:
-  peer: ${PEER:-thor-daemon.$PEER_NODE}
-  peerApi: ${PEER:-thor-api.$PEER_NODE}
   signer:
-    mnemonic: $MNEMONIC
+    mnemonic: $VALUE
 bifrost:
-  peer: ${PEER:-bifrost.$PEER_NODE}
-  binanceDaemon: ${PEER:-binance-daemon.$PEER_NODE:26660}
-  bitcoinDaemon: ${PEER:-bitcoin-daemon.$PEER_NODE:18443}
-  ethereumDaemon: ${PEER:-ethereum-daemon.$PEER_NODE:8545}
   signer:
-    mnemonic: $MNEMONIC
+    mnemonic: $VALUE
+EOF
+}
+
+set_ports () {
+  case $NET in
+    mocknet)
+      BINANCE_PORT=26657
+      BITCOIN_PORT=18443
+      ETHEREUM_PORT=8545
+      ;;
+    testnet)
+      BINANCE_PORT=26657
+      BITCOIN_PORT=18332
+      ETHEREUM_PORT=8545
+      ;;
+    mainnet)
+      BINANCE_PORT=27147
+      BITCOIN_PORT=8332
+      ETHEREUM_PORT=8545
+      ;;
+  esac
+}
+
+# creates validator config for a single node within the cluster
+create_validator_config () {
+  cat << EOF > $VALUES_VALIDATOR
+thor-daemon:
+  peer: $PEER
+  peerApi: $PEER_API
+  binanceDaemon: binance-daemon:$BINANCE_PORT
+
+bifrost:
+  peer: $PEER_BIFROST
+EOF
+}
+
+# creates validator config for multi nodes in the same cluster
+create_multi_validator_config () {
+  cat << EOF > $VALUES_VALIDATOR
+thor-daemon:
+  peer: thor-daemon.$NET-1
+  peerApi: thor-api.$NET-1
+  binanceDaemon: binance-daemon.$NET-1:$BINANCE_PORT
+
+bifrost:
+  peer: bifrost.$NET-1
+  binanceDaemon: http://binance-daemon.$NET-1:$BINANCE_PORT
+  bitcoinDaemon: bitcoin-daemon.$NET-1:$BITCOIN_PORT
+  ethereumDaemon: http://ethereum-daemon.$NET-1:$ETHEREUM_PORT
+
 binance-daemon:
   enabled: false
+
 bitcoin-daemon:
   enabled: false
+
 ethereum-daemon:
   enabled: false
 EOF
-			helm upgrade --install $RELEASE $SCRIPTPATH -n $NODE --create-namespace $FILE -f $SCRIPTPATH/tmp.yaml
-      rm $SCRIPTPATH/tmp.yaml
-		fi
-done
+}
+
+# remove temporary files
+clean_files () {
+  rm $VALUES_VALIDATOR $VALUES_MNEMONIC
+}
+
+# deploys a single node in the cluster
+deploy_single_node () {
+  create_mnemonic_config
+  create_validator_config
+  helm upgrade --install $NET $SCRIPTPATH -n $NET --create-namespace -f $VALUES -f $VALUES_MNEMONIC -f $VALUES_VALIDATOR
+}
+# deploys a multi nodes net within the cluster
+deploy_multi_node () {
+
+  create_multi_validator_config
+
+  for i in `seq 1 $NODES`; do
+    NODE=$NET-$i
+    echo Deploying $NODE
+
+    create_mnemonic_config
+
+    if [ $i = 1 ]; then
+      helm upgrade --install $NET $SCRIPTPATH -n $NODE --create-namespace -f $VALUES -f $VALUES_MNEMONIC
+    else
+      helm upgrade --install $NET $SCRIPTPATH -n $NODE --create-namespace -f $VALUES -f $VALUES_MNEMONIC -f $VALUES_VALIDATOR
+    fi
+  done
+}
+
+set_ports
+
+if [ -z $PEER ]; then
+  deploy_multi_node
+else
+  deploy_single_node
+fi
+
+clean_files
