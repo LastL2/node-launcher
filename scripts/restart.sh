527ef0438bf5842b7c34f0e2f4891862ae78d916
Fandral <fandral.dev@gmail.com>
2021-03-07 18:52:47 +0400

    Update restart command

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -6,12 +6,4 @@ get_node_net
 get_node_name
 get_node_service
 
-case $SERVICE in
-  midgard | midgard-timescaledb)
-    kubectl logs -f -n $NAME sts/$SERVICE
-    ;;
-  * )
-    kubectl logs -f -n $NAME deploy/$SERVICE
-    ;;
-esac
-exit 0
+kubectl delete -n $NAME pod -l app.kubernetes.io/name=$SERVICE
