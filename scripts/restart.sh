2bcf549cd3caaef82aa755eb96fdbeb9d7b1b127
crstin <x-git@crstin.com>
2021-03-08 19:10:03 +0100

    Schellcheck scripts

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -5,4 +5,4 @@ source ./scripts/core.sh
 get_node_info_short
 get_node_service
 
-kubectl delete -n $NAME pod -l app.kubernetes.io/name=$SERVICE
+kubectl delete -n "$NAME" pod -l app.kubernetes.io/name="$SERVICE"
