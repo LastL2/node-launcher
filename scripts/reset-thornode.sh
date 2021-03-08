82d5cc67432818098bb3f8df3576c46a625b4237
Fandral <fandral.dev@gmail.com>
2021-03-08 10:08:38 +0400

    Update warn messages

--- scripts/reset-thornode.sh
+++ scripts/reset-thornode.sh
@@ -4,8 +4,8 @@ source ./scripts/core.sh
 
 get_node_info_short
 
-echo !!! Destructive command, only apply in case of consensus failure on thor-daemon !!!
-echo Be careful, your thor-daemon data will be deleted, the service will restart and sync again
+warn "!!! Destructive command, only apply in case of consensus failure on thor-daemon !!!"
+warn "Be careful, your thor-daemon data will be deleted, the service will restart and sync again"
 confirm
 
 echo "=> Resetting THORNode"
