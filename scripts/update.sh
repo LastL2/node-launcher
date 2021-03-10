19ef1bed7f295d9e4e6263893fdf57900b6be8a8
Fandral <fandral.dev@gmail.com>
2021-03-10 08:52:07 +0400

    Fix typo

--- scripts/update.sh
+++ scripts/update.sh
@@ -13,7 +13,7 @@ fi
 source ./scripts/deploy.sh
 
 echo
-echo "=> Waiting thor THORNode daemon to be ready"
+echo "=> Waiting for THORNode daemon to be ready"
 sleep 20
 kubectl wait --for=condition=Ready pods -l app.kubernetes.io/name=thornode -n "$NAME" --timeout=5m
 
