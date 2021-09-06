cff8b97f75e655c2a7655bcd279eba2237e388bc
Fandral <fandral.dev@gmail.com>
2021-09-06 11:17:13 +0100

    Add name and confim on pause and resume cmd

--- scripts/resume.sh
+++ scripts/resume.sh
@@ -6,7 +6,9 @@ source ./scripts/core.sh
 
 get_node_info_short
 
-echo "=> Resuming node global halt"
+echo "=> Resuming node global halt from a THORNode named $boldyellow$NAME$reset"
+confirm
+
 kubectl exec -it -n "$NAME" deploy/thornode -- /kube-scripts/resume.sh >/dev/null
 sleep 5
 echo THORChain resumed
