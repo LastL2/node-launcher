4629c250bc8a8ed6e1c835f2833efc154160e3f7
pluto-9r <pluto@ninerealms.co>
2021-09-15 12:54:54 -0400

    use explicit container during exec

--- scripts/set-node-keys.sh
+++ scripts/set-node-keys.sh
@@ -7,6 +7,6 @@ source ./scripts/core.sh
 get_node_info_short
 
 echo "=> Setting THORNode keys"
-kubectl exec -it -n "$NAME" deploy/thornode -- /kube-scripts/set-node-keys.sh >/dev/null
+kubectl exec -it -n "$NAME" -c thornode deploy/thornode -- /kube-scripts/set-node-keys.sh >/dev/null
 sleep 5
 echo THORNode Keys updated
