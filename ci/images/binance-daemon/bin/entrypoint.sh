13e2f572ccbc1783aaf35f8a978e62615dc65c68
Ursa (9R) <ursa@ninerealms.co>
2022-01-09 00:07:46 +0000

    Faster Binance Sync

--- ci/images/binance-daemon/bin/entrypoint.sh
+++ ci/images/binance-daemon/bin/entrypoint.sh
@@ -12,6 +12,15 @@ EXE="ulimit -n 65535 && /usr/local/bin/bnbchaind start --home ${BNCHOME}"
 if [ ! -d "${BNCHOME}/config/" ]; then
   mkdir -p ${BNCHOME}/config/
   cp /node-binary/fullnode/${BNET}/config/* ${BNCHOME}/config/
+
+  if [[ "$BNET" == "prod" ]] && [[ "${RECOVER_NINEREALMS_SNAPSHOT:-true}" == "true" ]]; then
+    # if this is after fresh install or reset, use ninerealms persistent peer snapshot
+    sed -i 's/seeds = .*/seeds = ""/g' ${BNCHOME}/config/config.toml
+    sed -i 's/persistent_peers = .*/persistent_peers = "c908be15c76a11ef0fe95fae33f7d5d209b3a8bc@binance.ninerealms.com:27148"/g' ${BNCHOME}/config/config.toml
+    sed -i 's/private_peer_ids = .*/private_peer_ids = "c908be15c76a11ef0fe95fae33f7d5d209b3a8bc"/g' ${BNCHOME}/config/config.toml
+    sed -i 's/max_num_inbound_peers = .*/max_num_inbound_peers = 1/g' ${BNCHOME}/config/config.toml
+    sed -i 's/max_num_outbound_peers = .*/max_num_outbound_peers = 0/g' ${BNCHOME}/config/config.toml
+  fi
 fi
 chown -R bnbchaind:bnbchaind ${BNCHOME}/config/
 
--- ci/images/binance-daemon/bin/statesync-cleanup.sh
+++ ci/images/binance-daemon/bin/statesync-cleanup.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
-sleep 120  # wait a bit to initialize
+BINANCE_SEEDS=$(
+  curl -s --fail -m 10 https://dex.binance.org/api/v1/peers | \
+  jq '[.[] | select(.accelerated | not) | "\(.id)@\(.original_listen_addr)"] | join(",")'
+)
 
-while true; do
+while sleep 300; do
+
   echo "[statesync-cleanup] Checking for statesync cleanup"
+
+  SNAPSHOT_PULLED=$(curl -s --fail -m 10 http://localhost:27147/status | jq '.result.sync_info.index_height != "0"')
+  if [[ "${BNET:-prod}" == "prod" ]] && \
+    [[ "${RECOVER_NINEREALMS_SNAPSHOT:-true}" == "true" ]] && \
+    [[ "$SNAPSHOT_PULLED" == "true" ]] && \
+    grep -q 'persistent_peer.*binance.ninerealms.com' ${BNCHOME}/config/config.toml; then
+
+      # unset the ninerealms persistent peer
+      echo "[statesync-cleanup] Synced, unsetting ninerealms persistent peer..."
+      echo "[statesync-cleanup] Using new binance seeds: $BINANCE_SEEDS"
+      sed -i "s/seeds = \"\"/seeds = $BINANCE_SEEDS/g" ${BNCHOME}/config/config.toml
+      sed -i 's/persistent_peers = .*/persistent_peers = ""/g' ${BNCHOME}/config/config.toml
+      sed -i 's/max_num_inbound_peers = .*/max_num_inbound_peers = 40/g' ${BNCHOME}/config/config.toml
+      sed -i 's/max_num_outbound_peers = .*/max_num_outbound_peers = 10/g' ${BNCHOME}/config/config.toml
+
+      pkill -HUP bnbchaind  # the daemon does not handle HUP so this will still restart
+  fi
 
   # disable statesync reactor if synced
   SYNCED=$(curl -s --fail -m 10 http://localhost:27147/status | jq ".result.sync_info.catching_up | not")
   if [[ $SYNCED == "true" ]] && grep -q 'state_sync_reactor = true' ${BNCHOME}/config/config.toml; then
       echo "[statesync-cleanup] Synced, disabling statesync reactor..."
       sed -i "s/state_sync_reactor = true/state_sync_reactor = false/g" ${BNCHOME}/config/config.toml
+
       pkill -HUP bnbchaind  # the daemon does not handle HUP so this will still restart
   fi
 
@@ -20,6 +42,4 @@ while true; do
     rm -rf $snapshot
     sleep 5
   done
-
-  sleep 600
 done
