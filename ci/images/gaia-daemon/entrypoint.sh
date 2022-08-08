aaddfb607efed3798f3017c18bf5563a27a2f80f
Ursa (9R) <ursa@ninerealms.co>
2022-08-08 13:40:16 +0000

    Gaia 7.0.3 Image

--- ci/images/gaia-daemon/entrypoint.sh
+++ ci/images/gaia-daemon/entrypoint.sh
@@ -5,4 +5,4 @@ if [[ ! -f "/root/.gaia/config/app.toml" ]]; then
 	cp /etc/gaia/app.toml /root/.gaia/config/app.toml
 fi
 
-exec /gaiad start --rpc.laddr tcp://0.0.0.0:26657 --x-crisis-skip-assert-invariants "$@"
+exec /gaiad start --log_format json --rpc.laddr tcp://0.0.0.0:26657 --x-crisis-skip-assert-invariants "$@"
