d1714d66555b57a5adaae573f9a823cf5271c2b0
Ursa <ursa@ninerealms.co>
2022-08-31 09:12:26 -0400

    Fix Make Status

--- scripts/votes.sh
+++ scripts/votes.sh
@@ -7,7 +7,7 @@ VOTE_REMINDER_THRESHOLD=10
 # VOTE_REMINDER_BLACKLIST contains the set of mimir keys we will never prompt
 # for a vote.
 VOTE_REMINDER_BLACKLIST=$(
-  cat <<EOF | sed -e ':a;N;$!ba;s/^/"/;s/\n/","/g;s/$/"/'
+  cat <<EOF | awk -v q="\"" '{print q$1q}' | tr "\n" "," | sed 's/.$//'
 CLOUDPROVIDERLIMIT
 ENABLEUPDATEMEMOTERRA
 HALTGAIACHAIN
