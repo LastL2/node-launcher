75a91095cfa6a530e1e6b4b4048404c74c97187c
Ursa <ursa@ninerealms.co>
2022-09-28 10:42:51 -0400

    Update Homebrew Instructions for MacOS GNU Utilities

--- scripts/check-environment.sh
+++ scripts/check-environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 check_gnu() {
-  $1 --version | head -n 1 | grep -q "GNU" && return
+  $1 --version 2>/dev/null | head -n 1 | grep -q "GNU" && return
   echo "GNU $1 is required." >/dev/tty
   FAILED=1
 }
@@ -11,11 +11,21 @@ check_gnu awk
 check_gnu find
 check_gnu sed
 
+# check make is version 4+
+if [ -z "$FAILED" ]; then
+  MAKE_VERSION=$(make --version 2>/dev/null | head -n 1 | awk -F '[ \\.]' '{print $3}')
+  if [ "$MAKE_VERSION" -lt 4 ]; then
+    echo "make version 4+ is required." >/dev/tty
+    FAILED=1
+  fi
+fi
+
 if [ -n "$FAILED" ]; then
   if [ "$(uname)" == "Darwin" ]; then
     echo >/dev/tty
-    echo "Mac OS can try the following to update native utilities to GNU version (homebrew):" >/dev/tty
-    echo "brew install coreutils binutils diffutils findutils gnu-tar gnu-sed gawk grep" >/dev/tty
+    echo "Mac OS can try the following to update native utilities to the latest GNU version (homebrew):" >/dev/tty
+    echo "1. Run: brew install coreutils binutils diffutils findutils gnu-tar gnu-sed gawk grep make" >/dev/tty
+    echo "2. Follow the instructions from the brew output to update your PATH so the GNU utilities are default" >/dev/tty
   fi
   echo >/dev/tty
   exit 1
