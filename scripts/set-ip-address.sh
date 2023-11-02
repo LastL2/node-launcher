b3c726d4c5c6948a9aa114c9ee9ee4ef4cd83274
Son of Odin <2700975-son-of-odin@users.noreply.gitlab.com>
2023-11-02 03:18:21 +0000

    set custom IP address

--- scripts/set-ip-address.sh
+++ scripts/set-ip-address.sh
@@ -6,7 +6,10 @@ source ./scripts/core.sh
 
 get_node_info_short
 
+GATEWAY=$(kubectl -n "${NAME}" get configmap gateway-external-ip -o jsonpath="{.data.externalIP}")
+IP_ADDRESS="${IP_ADDRESS:-${GATEWAY}}"
+
 echo "=> Setting THORNode IP address"
-kubectl exec -it -n "$NAME" deploy/thornode -- /kube-scripts/set-ip-address.sh "$(kubectl -n "$NAME" get configmap gateway-external-ip -o jsonpath="{.data.externalIP}")"
+kubectl exec -it -n "$NAME" deploy/thornode -- /kube-scripts/set-ip-address.sh "$IP_ADDRESS"
 sleep 5
-echo THORNode IP address updated
+echo THORNode IP address updated to "${IP_ADDRESS}"
